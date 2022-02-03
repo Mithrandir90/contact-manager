ccea7e230e0d5631004901682d3a44a7324beccd
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2022-02-03 13:02:14 +0000

    contact manager working properly

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
@@ -11,7 +12,14 @@ function App() {
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuidv4(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
   };
 
   useEffect(() => {
@@ -27,7 +35,7 @@ function App() {
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
