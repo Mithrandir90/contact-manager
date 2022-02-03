b068ec6328d64255a62fbb7813a516df7fb1dc28
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2022-02-03 11:36:19 +0000

    basic style finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import React from "react";
+import { useState } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {};
+
   return (
-    <div>
+    <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
